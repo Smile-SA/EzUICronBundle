056a2c4eb56087a051c9b58f1e12c4142632325d
Emmanuel Drouet <emmanuel.drouet@gmail.com>
2017-01-08 23:36:03 -0800

    create cron lists cancel edit button

--- Resources/public/js/views/smileez-uicron-view.js
+++ Resources/public/js/views/smileez-uicron-view.js
@@ -5,6 +5,9 @@ YUI.add('smileez-uicron-view', function (Y) {
         events: {
             '.smile-cron-edit': {
                 'tap': '_editField'
+            },
+            '.smile-cron-cancel': {
+                'tap': '_cancelEditField'
             }
         },
 
@@ -19,10 +22,30 @@ YUI.add('smileez-uicron-view', function (Y) {
                 cronValueNode = this.get('container').one('#smile-cron-value-' + type + '-' + alias),
                 cronTdNode = this.get('container').one('#smile-cron-td-' + type + '-' + alias);
 
-            cronTdNode.appendChild(Y.Node.create('<input type="text" size="10" id="' + type + '-' + alias + '" class="smile-cron-input" value="' + cronValueNode.getHTML().trim() + '">'));
-            cronTdNode.appendChild(Y.Node.create('<input type="button" data-type="' + type + '" data-alias="' + alias + '" class="smile-cron-input" value="' + Y.eZ.trans('smileezuicron.button.cancel', {}, 'smileezcron') + '">'));
-            cronTdNode.appendChild(Y.Node.create('<input type="button" data-type="' + type + '" data-alias="' + alias + '" class="smile-cron-input" value="' + Y.eZ.trans('smileezuicron.button.save', {}, 'smileezcron') + '">'));
-            cronValueNode.addClass('editView');
+            if (cronTdNode) {
+                cronTdNode.appendChild(Y.Node.create('<div id="smile-cron-container-' + type + '-' + alias + '"></div>'));
+
+                var cronContainerNode = this.get('container').one('#smile-cron-container-' + type + '-' + alias);
+
+                cronContainerNode.appendChild(Y.Node.create('<input type="text" size="10" id="' + type + '-' + alias + '" class="smile-cron-input" value="' + cronValueNode.getHTML().trim() + '">'));
+                cronContainerNode.appendChild(Y.Node.create('<input type="button" data-type="' + type + '" data-alias="' + alias + '" class="smile-cron-cancel" value="' + Y.eZ.trans('smileezuicron.button.cancel', {}, 'smileezcron') + '">'));
+                cronContainerNode.appendChild(Y.Node.create('<input type="button" data-type="' + type + '" data-alias="' + alias + '" class="smile-cron-save" value="' + Y.eZ.trans('smileezuicron.button.save', {}, 'smileezcron') + '">'));
+                cronValueNode.addClass('editView');
+            }
+        },
+
+        _cancelEditField: function (e) {
+            e.preventDefault();
+            var type = e.target.getAttribute('data-type'),
+                alias = e.target.getAttribute('data-alias'),
+                cronValueNode = this.get('container').one('#smile-cron-value-' + type + '-' + alias),
+                cronTdNode = this.get('container').one('#smile-cron-td-' + type + '-' + alias),
+                cronContainerNode = this.get('container').one('#smile-cron-container-' + type + '-' + alias);
+
+            if (cronTdNode) {
+                cronValueNode.removeClass('editView');
+                cronTdNode.removeChild(cronContainerNode);
+            }
         },
     });
 });
